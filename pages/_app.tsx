c700c6c7b940a2c64e2fcb58d8cf4c59a2a99e9f
jared <jaredcbaum@gmail.com>
2023-06-28 12:14:18 -0500

    chore(styles): styles to src dir, ts aliases, fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,25 @@ import type { AppProps } from "next/app";
 import "modern-normalize/modern-normalize.css";
 import "@ionic-internal/design-system/dist/reset/index.css";
 import "@ionic-internal/design-system/dist/tokens/index.css";
-import "@/styles/ds.css";
+import "@styles/ds.css";
+
+import "@styles/global.css";
+
+import { clsx } from "clsx";
+
+import { inter, robotoMono } from "@assets/fonts";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div
+      className={clsx(
+        "font-root",
+        inter.className,
+        inter.variable,
+        robotoMono.variable
+      )}
+    >
+      <Component {...pageProps} />
+    </div>
+  );
 }
