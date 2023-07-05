161a25558a4d1a12882d7a7e9546cc4b68137e7a
jared <jaredcbaum@gmail.com>
2023-07-05 17:25:29 -0500

    chore(capacitor-site): newsletter signup component

---
+++ src/components/newsletter-signup/index.tsx
@@ -0,0 +1,29 @@
+import { HubspotForm } from "@ionic-internal/components-react";
+import clsx from "clsx";
+
+import styles from "./index.module.scss";
+
+const NewsletterSignup = () => {
+  return (
+    <div
+      className={clsx(styles.newsletter, "newsletter-signup", "ds-container")}
+    >
+      <div className="wrapper">
+        <div className="heading-group">
+          <h3>The latest updates. Delivered monthly.</h3>
+          <p>
+            Capacitor is getting better every day. Sign up for a monthly email
+            on the latest updates, releases, articles, and news!
+          </p>
+        </div>
+        <div className="form-group">
+          <HubspotForm
+            createProps={{ formId: "c8d355e3-a5ad-4f91-a2c0-c9dc93e10658" }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsletterSignup;
