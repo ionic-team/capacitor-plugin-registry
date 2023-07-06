42d8e90892b0c7201e42510aef0d2c62403719fd
jared <jaredcbaum@gmail.com>
2023-07-06 11:22:57 -0500

    chore(capacitor-site): preact solution page

---
+++ pages/solution/preact.tsx
@@ -0,0 +1,9 @@
+import SolutionPreactPage from "@/src/pages/solution/preact/index";
+
+import { ComponentProps } from "react";
+
+const SolutionPreact = (props: ComponentProps<"div">) => (
+  <SolutionPreactPage {...props} />
+);
+
+export default SolutionPreact;
