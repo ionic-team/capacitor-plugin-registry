cbc2307047228f984b033c6e1315def4ff8fd4bf
jared <jaredcbaum@gmail.com>
2023-07-06 11:24:16 -0500

    chore(capacitor-site): stencil solution page

---
+++ pages/solution/stencil.tsx
@@ -0,0 +1,9 @@
+import SolutionStencilPage from "@/src/pages/solution/stencil/index";
+
+import { ComponentProps } from "react";
+
+const SolutionStencil = (props: ComponentProps<"div">) => (
+  <SolutionStencilPage {...props} />
+);
+
+export default SolutionStencil;
