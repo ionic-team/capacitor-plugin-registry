810acbaaf3efab9509d57a68cba2524d11da88d4
jared <jaredcbaum@gmail.com>
2023-07-06 11:50:58 -0500

    chore(capacitor-site): solution assets and layout component

---
+++ src/pages/solution/components/layout/index.tsx
@@ -0,0 +1,48 @@
+import clsx from "clsx";
+
+import styles from "./index.module.scss";
+import NewsletterSignup from "@components/newsletter-signup";
+import Prefooter from "@components/prefooter";
+import SiteFooter from "@components/site-footer";
+import Image from "next/image";
+
+const SolutionPageLayout = ({
+  children,
+  framework,
+}: {
+  framework: {
+    id: string;
+    name: string;
+    theme: string;
+    logo: string;
+  };
+}) => {
+  const { src, width, height } = framework.logo;
+  return (
+    <>
+      <main className={styles.page}>
+        <section className={clsx(styles.top, "ds-container")}>
+          <div className="heading-group">
+            <Image
+              width={width}
+              height={height}
+              src={src}
+              alt={framework.name + " logo"}
+              className="react"
+            />
+            <h1 className="ds-heading-2">{framework.name} &amp; Capacitor</h1>
+            <p className="ds-paragraph-2">
+              Build native mobile apps with web technology and {framework.name}
+            </p>
+          </div>
+        </section>
+        {children}
+        <NewsletterSignup />
+        <Prefooter />
+      </main>
+      <SiteFooter />
+    </>
+  );
+};
+
+export default SolutionPageLayout;
