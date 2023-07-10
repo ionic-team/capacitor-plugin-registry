29eb430ea0f4ab598d57dce11fd3961adc852abb
jared <jaredcbaum@gmail.com>
2023-07-10 16:27:28 -0600

    chore(capacitor-site): add isWebLink utility fn

---
+++ src/utils/prismic.ts
@@ -0,0 +1,5 @@
+import { FilledLinkToWebField, LinkField, LinkType } from "@prismicio/types";
+
+export function isWebLink(field: LinkField): field is FilledLinkToWebField {
+  return field?.link_type === LinkType["Web"];
+}
