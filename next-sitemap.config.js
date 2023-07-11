bbc563f3429d4b79b6c630dc2e36098a101664f8
jared <jaredcbaum@gmail.com>
2023-07-10 18:01:21 -0600

    chore(capacitor-site): sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteUrl: 'https://capacitorjs.com',
+  changefreq: 'weekly',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://capacitorjs.com/docs/sitemap.xml'],
+  },
+};
